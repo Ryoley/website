b616672f8b0fd5ad0c6ba3c4cf087c4ba54ce8c3
Paul Chevrier <paul.chevrier@gmail.com>
2020-12-22 18:13:25 +0100

    edit home page

--- homePage/home.js
+++ homePage/home.js
@@ -1,19 +1,5 @@
-element = document.getElementById("animate");
+$(".menu").click(function () {
+    $(this).parent().toggleClass("close");
+});
 
-if (element) {
-    // reset the transition by...
-    element.addEventListener("click", function (e) {
-        e.preventDefault;
-
-        console.log('element', element.classList);
-
-        // removing the class
-        element.classList.remove("run-animation");
-
-        // triggering reflow
-        void element.offsetWidth;
-
-        // and re-adding the class
-        element.classList.add("run-animation");
-    }, false);
-}
+ //Based on: https://dribbble.com/shots/3377940-Home-Budget-App-Interactions
